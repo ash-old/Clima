d4997a1ef185ee294ed1a11ca3cf1149d8775538
ash-old <ashley.oldham@gmail.com>
2021-06-05 01:09:16 +0100

    setting up delegate methods to capture the data from the searchTextField.

--- Clima/Controller/ViewController.swift
+++ Clima/Controller/ViewController.swift
@@ -7,18 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
  
   @IBOutlet weak var conditionImageView: UIImageView!
   @IBOutlet weak var temperatureLabel: UILabel!
   @IBOutlet weak var cityLabel: UILabel!
+  @IBOutlet weak var searchTextField: UITextField!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    //textField should report back to ViewController
+    searchTextField.delegate = self
   }
 
-
+  @IBAction func searchPressed(_ sender: UIButton) {
+    searchTextField.endEditing(true)
+//    print(searchTextField.text!)
+    
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    searchTextField.endEditing(true)
+//    print(searchTextField.text!)
+    return true
+  }
+  
+  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    if textField.text != "" {
+      return true
+    } else {
+      searchTextField.placeholder = "Search a City"
+      return false
+    }
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    print(searchTextField.text!)
+    searchTextField.text = ""
+  }
+  
 }
 
